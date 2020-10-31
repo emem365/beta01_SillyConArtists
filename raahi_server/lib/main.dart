a8fcf31c86d67db03d53af22f092f0f662540d12
Madhur Maurya <madhurmaurya365@gmail.com>
2020-11-01 04:20:38 +0530

    SMS Service added

--- raahi_server/lib/main.dart
+++ raahi_server/lib/main.dart
@@ -2,19 +2,27 @@ import 'package:chopper/chopper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:raahi_server/api_service.dart';
-import 'package:raahi_server/key.dart';
+import 'package:raahi_server/app_state.dart';
+import 'package:raahi_server/messages.dart';
 import 'package:raahi_server/models.dart';
+import 'package:polyline/polyline.dart';
+import 'package:raahi_server/service_locator.dart';
+import 'package:sms_maintained/sms.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SmsReceiver receiver = SmsReceiver();
+  receiver.onSmsReceived.listen(onMessageReceived);
+  setupLocator();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider(
-      create: (_) => ApiService.create(),
-      dispose: (_, service) => service.client.dispose(),
+    return Provider<AppState>(
+      create: (_) => AppState(),
+      dispose: (_, appState) => appState.apiService.client.dispose(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
@@ -37,25 +45,82 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Response<DirectionsResponse> response;
+  Polyline decodedpoints;
+  String startLat;
+  String endLat;
+  String startLon;
+  String endLon;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-          child: RaisedButton(
-              onPressed: () async {
-                ApiService service =
-                    Provider.of<ApiService>(context, listen: false);
-                // Response<DirectionsResponse> response = await service.getDirections(
-                //     '8.681495,49.41461', '8.687872,49.420318');
-                Response response = await service.getGeoCode(
-                    'Bhopal', '23.249788','77.415637');
-                    
-                print(response.body.toString().substring(100));
-              },
-              child: Text('Press me to print to debug'))),
+      body: SingleChildScrollView(
+          child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              keyboardType: TextInputType.number,
+              onSaved: (val) => startLat = val,
+              decoration: InputDecoration(labelText: 'Start Latitude'),
+            ),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              onSaved: (val) => startLon = val,
+              decoration: InputDecoration(labelText: 'Start Longitude'),
+            ),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              onSaved: (val) => endLat = val,
+              decoration: InputDecoration(labelText: 'End Latitude'),
+            ),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              onSaved: (val) => endLon = val,
+              decoration: InputDecoration(labelText: 'End Longitude'),
+            ),
+            RaisedButton(
+                onPressed: () async {
+                  _formKey.currentState.save();
+
+                  ApiService service =
+                      Provider.of<ApiService>(context, listen: false);
+                  response = await service.getDirections(
+                      '$startLat,$startLon', '$endLat,$endLon');
+                  final points = response
+                      .body.features.first.geometry.coordinates
+                      .map((coord) => coord.toList())
+                      .toList();
+                  final poly =
+                      Polyline.Encode(decodedCoords: points, precision: 5);
+                  decodedpoints = Polyline.Decode(
+                      encodedString: poly.encodedString, precision: 5);
+                  print(
+                      'String: ${poly.encodedString.length} ${points.length}');
+                  setState(() {});
+                },
+                child: Text('Press me ')),
+            SizedBox(height: 25),
+            if (decodedpoints != null)
+              ...decodedpoints.decodedCoords
+                  .map((e) => ListTile(title: Text(e.toString())))
+                  .toList(),
+            Divider(),
+            if (response != null)
+              ...response.body.features.first.properties.segments.first.steps
+                  .map<ListTile>((Steps step) => ListTile(
+                        title: Text(step.instruction),
+                      ))
+                  .toList(),
+          ],
+        ),
+      )),
     );
   }
 }
+
---
+++ raahi_server/lib/messages.dart
@@ -0,0 +1,114 @@
+import 'dart:math';
+
+import 'package:polyline/polyline.dart';
+import 'package:raahi_server/app_state.dart';
+import 'package:raahi_server/models.dart';
+import 'package:raahi_server/service_locator.dart';
+import 'package:sms_maintained/sms.dart';
+
+void onMessageReceived(SmsMessage message) {
+  String messageCode = message.body.substring(0, 3);
+  print('message: ${message.body}');
+  switch (messageCode) {
+    case 'R0@':
+      // For Initial Query Statement: R0@queryString;latitude;longitude
+      // Will return Suggested Locations String: S0@placeName;placeCity;placeName;placeCity;placeName;placeCity
+      // limited to three suggestions
+      processR0String(message);
+      break;
+    case 'R1@':
+      // Once the user has seen the suggestions, the must select a location by specifying index
+      // Set a Destination. Input String:  R1@index;lat;lon
+      // between 0<=index<3
+
+      // Since the user has selected now he gets 2 outputs
+      // 1. 3 steps on the path
+      // String: S1@instruction;instructionType;distance;instruction;instructionType;distance;instruction;instructionType;distance;
+      // limited to 3 instructions in one time. instruction Type is a value for showing arrow
+      // 2. Point polyline String limited to 30 points, skipping alternate points; 5 decimal spaces
+      // Output String : S2@string
+      processR1String(message);
+      break;
+    case 'R2@':
+      // Get Further Path. We call the api again with a new lat long but old destination location
+      // So that we may Stay on Path better?
+      // Input String R2@latitude;longitude
+      // Output would be S1 and S2 again
+      processR2String(message);
+      break;
+    default:
+    //ignore message
+
+  }
+}
+
+void processR0String(SmsMessage message) async {
+  String str = message.body.substring(3);
+  List<String> parts = str.split(';');
+  if (parts.length == 3) {
+    List<GeoCodeFeature> response = await locator<AppState>()
+        .query(message.address, parts[0], parts[1], parts[2])
+        .catchError((e) {
+      print('Error');
+    });
+    String responseString = 'S0@';
+    response?.sublist(0, min(3, response.length))?.forEach((element) {
+      responseString +=
+          '${element.properties.name};${element.properties.county}';
+    });
+    if (responseString == 'S0@') responseString = 'ERROR';
+    locator<SmsSender>().sendSms(SmsMessage(message.address, responseString));
+  }
+  //else ignore?
+}
+
+void processR1String(SmsMessage message) async {
+  String str = message.body.substring(3);
+  try {
+    List<String> parts = str.split(';');
+    if (parts.length == 3) {
+      int index = int.parse(parts[0]);
+      if (index >= 0 && index < 3) {
+        locator<AppState>().setDestination(message.address, index);
+        await sendPath(message, parts[1], parts[2]);
+      }
+    }
+  } catch (e) {}
+  //else ignore?
+}
+
+void processR2String(SmsMessage message) async {
+  String str = message.body.substring(3);
+  try {
+    List<String> parts = str.split(';');
+    if (parts.length == 2) {
+      await sendPath(message, parts[0], parts[1]);
+    }
+  } catch (e) {}
+  //else ignore?
+}
+
+Future sendPath(SmsMessage message, String lat, String lon) async {
+  try {
+    List<Steps> steps =
+        await locator<AppState>().getpath(message.address, lat, lon);
+    String pathString = 'S1@';
+    steps?.sublist(0, min(3, steps.length))?.forEach((element) {
+      pathString +=
+          '${element.instruction};${element.type};${element.distance}';
+    });
+    if (pathString == 'S0@') throw Exception();
+    locator<SmsSender>().sendSms(SmsMessage(message.address, pathString));
+    List<List<double>> points =
+        locator<AppState>().getPathPoints(message.address);
+    points = points.sublist(0, min(60, points.length));
+    List<List<double>> pointsSkipAlt = List();
+    for (int i = 0; i < points.length; i++) {
+      if (i % 2 == 0) pointsSkipAlt.add(points[i]);
+    }
+
+    final poly = Polyline.Encode(decodedCoords: pointsSkipAlt, precision: 5);
+    locator<SmsSender>()
+        .sendSms(SmsMessage(message.address, poly.encodedString));
+  } catch (e) {}
+}
