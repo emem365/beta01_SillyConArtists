f545e134a110f5a4b03cb4269b2ee073fc3a2a5e
Madhur Maurya <madhurmaurya365@gmail.com>
2020-11-01 07:08:28 +0530

    UI-server

--- raahi_server/lib/main.dart
+++ raahi_server/lib/main.dart
@@ -1,130 +1,137 @@
-import 'package:chopper/chopper.dart';
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:raahi_server/api_service.dart';
-import 'package:raahi_server/app_state.dart';
-import 'package:raahi_server/messages.dart';
-import 'package:raahi_server/models.dart';
-import 'package:polyline/polyline.dart';
-import 'package:raahi_server/service_locator.dart';
-import 'package:sms_maintained/sms.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SmsReceiver receiver = SmsReceiver();
-  receiver.onSmsReceived.listen(onMessageReceived);
-  setupLocator();
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+var colorList = [
+  ColorInfo("purple", Colors.purple, Colors.purple[500].toString()),
+  ColorInfo("orange", Colors.orange, Colors.orange[500].toString()),
+  ColorInfo("red", Colors.red, Colors.red[500].toString()),
+  ColorInfo("green", Colors.green, Colors.green[500].toString()),
+  ColorInfo("blue", Colors.blue, Colors.blue[500].toString()),
+  ColorInfo("yellow", Colors.yellow, Colors.yellow[500].toString()),
+  ColorInfo("purple", Colors.purple, Colors.purple[500].toString()),
+  ColorInfo("amber", Colors.amber, Colors.amber[500].toString()),
+  ColorInfo("cyan", Colors.cyan, Colors.cyan[500].toString()),
+  ColorInfo("grey", Colors.grey, Colors.grey[500].toString()),
+  ColorInfo("teal", Colors.teal, Colors.teal[500].toString()),
+  ColorInfo("pink", Colors.pink, Colors.pink[500].toString()),
+  ColorInfo("orange", Colors.orange, Colors.orange[500].toString()),
+  ColorInfo("teal", Colors.teal, Colors.teal[500].toString()),
+  ColorInfo("green", Colors.green, Colors.green[500].toString()),
+  ColorInfo("cyan", Colors.cyan, Colors.cyan[500].toString()),
+];
+
+class ColorInfo {
+  String name;
+  MaterialColor color;
+  String hex;
+  ColorInfo(this.name, this.color, this.hex);
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Provider<AppState>(
-      create: (_) => AppState(),
-      dispose: (_, appState) => appState.apiService.client.dispose(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
-    );
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double iter = 0.0;
+
+  var colors = List.generate(
+      20, (index) => colorList[Random().nextInt(colorList.length)]);
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 1)).then((value) async {
+      for (int i = 0; i < 2000000; i++) {
+        setState(() {
+          iter = iter + 0.00001;
+        });
+        await Future.delayed(Duration(milliseconds: 50));
+      }
+    });
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Response<DirectionsResponse> response;
-  Polyline decodedpoints;
-  String startLat;
-  String endLat;
-  String startLon;
-  String endLon;
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
-          child: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
           children: [
-            TextFormField(
-              keyboardType: TextInputType.number,
-              onSaved: (val) => startLat = val,
-              decoration: InputDecoration(labelText: 'Start Latitude'),
+            Container(
+              child: CustomPaint(
+                painter: DemoPainter(iter, colors),
+                child: Container(),
+              ),
             ),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              onSaved: (val) => startLon = val,
-              decoration: InputDecoration(labelText: 'Start Longitude'),
+            Center(
+              child: Material(
+                color: Colors.white54,
+                child: SizedBox(
+                  height: 200,
+                  width: 300,
+                  child: Center(
+                      child: Text(
+                    'Raahi-server',
+                    style: Theme.of(context).textTheme.headline3.copyWith(
+                        color: Colors.black, fontStyle: FontStyle.italic),
+                  )),
+                ),
+              ),
             ),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              onSaved: (val) => endLat = val,
-              decoration: InputDecoration(labelText: 'End Latitude'),
-            ),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              onSaved: (val) => endLon = val,
-              decoration: InputDecoration(labelText: 'End Longitude'),
-            ),
-            RaisedButton(
-                onPressed: () async {
-                  onMessageReceived(SmsMessage(
-                    '+917009937626',
-                    'R1@0;28.7314161;77.0376372'
-                  ));
-                  // _formKey.currentState.save();
-
-                  // ApiService service =
-                  //     Provider.of<ApiService>(context, listen: false);
-                  // response = await service.getDirections(
-                  //     '$startLat,$startLon', '$endLat,$endLon');
-                  // final points = response
-                  //     .body.features.first.geometry.coordinates
-                  //     .map((coord) => coord.toList())
-                  //     .toList();
-                  // final poly =
-                  //     Polyline.Encode(decodedCoords: points, precision: 5);
-                  // decodedpoints = Polyline.Decode(
-                  //     encodedString: poly.encodedString, precision: 5);
-                  // print(
-                  //     'String: ${poly.encodedString.length} ${points.length}');
-                  // setState(() {});
-                },
-                child: Text('Press me ')),
-            SizedBox(height: 25),
-            if (decodedpoints != null)
-              ...decodedpoints.decodedCoords
-                  .map((e) => ListTile(title: Text(e.toString())))
-                  .toList(),
-            Divider(),
-            if (response != null)
-              ...response.body.features.first.properties.segments.first.steps
-                  .map<ListTile>((Steps step) => ListTile(
-                        title: Text(step.instruction),
-                      ))
-                  .toList(),
           ],
         ),
-      )),
+      ),
     );
   }
 }
 
+class DemoPainter extends CustomPainter {
+  double iter;
+  List<ColorInfo> colors;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    renderDrawing(canvas, size);
+  }
+
+  void renderDrawing(Canvas canvas, Size size) {
+    canvas.drawPaint(Paint()..color = Colors.black87);
+
+    renderStructure2(canvas, size, iter, colors, 9);
+  }
+
+  void renderStructure2(Canvas canvas, Size size, double iter,
+      List<ColorInfo> colors, int totalIter) {
+    for (int j = 0; j < totalIter; j++) {
+      double distance = 0.0;
+      double tempIter = totalIter - j + iter;
+
+      for (double i = 0; i < 80; i = i + 0.01) {
+        canvas.drawCircle(
+          Offset(
+            (size.width / 2) + (distance * cos(distance) * sin(distance)),
+            (size.height / 2) + (distance * tan(distance) * sin(distance)),
+          ),
+          5.0 - (0.5 * (totalIter - tempIter)),
+          Paint()..color = colors[j].color,
+        );
+
+        distance = distance + (0.2 + (0.1 * totalIter - tempIter));
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(DemoPainter oldDelegate) {
+    return iter != oldDelegate.iter;
+  }
+
+  DemoPainter(this.iter, this.colors);
+}
